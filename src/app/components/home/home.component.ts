4726e05a6f6df1d847d1dc0e3eebbb1ed0d8a153
Herbert Pro <hprocastillo@gmail.com>
2024-03-06 07:56:22 -0500

    cuarto commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,19 +7,18 @@ import {Timestamp} from "firebase/firestore";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    ReactiveFormsModule
-  ],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
-  requestForm: FormGroup;
+  public requestForm: FormGroup;
+  public messageFormType: string = "";
+  public messageFormContent: string = "";
 
   constructor(private fb: FormBuilder, private requestService: RequestsService) {
     this.requestForm = this.fb.group({
-      fullName: ['',[Validators.required]],
+      fullName: ['', [Validators.required]],
       email: ['', [Validators.required]],
       phone: ['', [Validators.required]],
       content: ['', [Validators.required]],
@@ -27,20 +26,47 @@ export class HomeComponent {
   }
 
   sendRequest() {
-    let newRequest: Request;
-    console.log("Formulario" + this.requestForm.value);
-
     if (this.requestForm.valid) {
+      let newRequest: Request;
       newRequest = this.requestForm.value;
       newRequest.createdAt = Timestamp.fromDate(new Date());
-      this.requestService.addRequest(newRequest)
-        .then(res => {
-          console.log(res);
-        })
-        .catch(e => {
-          console.log(e);
-        });
+
+      try {
+        this.requestService.addRequest(newRequest)
+          .then(res => {
+            console.log(res);
+            this.messageFormType = "success";
+            this.messageFormContent = "¡Su consulta fue enviada con éxito! Nos comunicaremos lo antes posible.";
+            this.requestForm.reset();
+            setTimeout(() => {
+              this.messageFormType = "";
+              this.messageFormContent = "";
+            }, 5000);
+
+          })
+          .catch(e => console.log(e));
+
+      } catch (e) {
+        console.log(e);
+        this.messageFormType = "danger";
+        this.messageFormContent = "Su mensaje no pudo ser enviado. Inténtelo nuevamente.";
+        this.requestForm.reset();
+        setTimeout(() => {
+          this.messageFormType = "";
+          this.messageFormContent = "";
+        }, 5000);
+
+      }
+    } else {
+      this.messageFormType = "warning";
+      this.messageFormContent = "Debe completar todos los campos del formulario.";
+      setTimeout(() => {
+        this.messageFormType = "";
+        this.messageFormContent = "";
+      }, 5000);
+
     }
+
   }
 
 }
